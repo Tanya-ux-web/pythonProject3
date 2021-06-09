1b52384fba958e5d3ba04b0940e3ca58ff5eefcf
tatiana <zaharovatany110401@gmail.com>
2021-06-09 21:16:34 +0300

    еще проверка

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm изменение проверка ')
-
+    print_hi('PyCharm изменение проверка')
+print_hi('PyCharm изменение проверка')
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
